d8fcf08a48267f4421edf45f509ce1b0338e2bdc
casinca4 <s.antonelli@web.de>
2020-02-09 12:12:28 +0100

    Build the Movies component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movies from './components/Movies';
 
 function App() {
-  return (
-  <main className="container">
-    <h1>Hello !</h1>
-  </main>
-  );
+	return (
+		<main className="container">
+			<Movies />
+		</main>
+	);
 }
 
 export default App;
